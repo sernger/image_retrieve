68402c8b69cd70e03a2cb528ac37e7a5cb282196
sernger <sernger09@gmail.com>
2020-01-02 17:19:38 +0800

    remove comment

--- train_res.py
+++ train_res.py
@@ -5,6 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import keras
 import keras.backend as K
+from keras.utils import plot_model
+from keras.models import Model
+
 
 
 from dataset import MNISTDataset, ChemicalDataset
@@ -21,14 +24,12 @@ flags.DEFINE_string('model', 'mnist', 'model to run')
 
 K.set_image_data_format('channels_last')
 def ResNet50_model(input_shape):
-    x = keras.Input(shape=input_shape)
-    y = keras.applications.ResNet50(include_top=False, input_tensor=x, weights=None, pooling='max')(x)
-    #y = keras.layers.Flatten()(y)
-    return keras.Model(inputs=x, outputs=y)
+    res_model = keras.applications.ResNet50(include_top=False, input_shape=input_shape, weights=None, pooling='max')
+   # plot_model(res_model,to_file="Resnet50.png",show_shapes=True)
+    return res_model
 
 def contrastive_loss(l, r, y):
     margin = 0.5
-   # l, r, y = y_pred
     distance = K.sqrt(K.sum(K.pow(l - r, 2), 1, keepdims=True))
     similarity = y * K.square(distance)                                           # keep the similar label (1) close to each other
     dissimilarity = (1 - y) * K.square(K.maximum((margin - distance), 0))        # give penalty to dissimilar label if the distance is bigger than margin
@@ -42,25 +43,18 @@ def next_generator():
 
 if __name__ == "__main__":
 
-
-    #dataset = ChemicalDataset()
     input_shape = [160,160,1]
-    #list(dataset.images_train.shape[1:])
     print("input_shape:", input_shape)
     colors = ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ff00ff', '#990000', '#999900', '#009900', '#009999']
 
     left = keras.Input(shape=input_shape, name='left')
     right = keras.Input(shape=input_shape, name='right')
-    #left = tf.placeholder(tf.float32, placeholder_shape, name='left')
-    #right = tf.placeholder(tf.float32, placeholder_shape, name='right')
+
     model = ResNet50_model(input_shape)
     left_out = model(left)
     right_out = model(right)
 
-
     label = keras.Input(shape=(1,), name='label')
-   # label = tf.placeholder(tf.int32, [None, 1], name='label') # 1 if same, 0 if different
-    #label_float = keras.backend.cast_to_floatx(label)
 
     #custom loss layer
     loss=keras.layers.Lambda(lambda x:contrastive_loss(*x), name="loss")([left_out, right_out, label])
@@ -70,6 +64,11 @@ if __name__ == "__main__":
     loss_layer = siamese_model.get_layer("loss").output
     model.add_loss(loss_layer)
     siamese_model.compile(optimizer='Adam', loss=[None,None,None])
+
+    #plot_model(siamese_model, to_file="siamese_model_expand.png", show_shapes=True, expand_nested=True)
+    plot_model(siamese_model, to_file="siamese_model_expand.png", show_shapes=True)
+
+    print(siamese_model.summary())
 
     data_gen = next_generator()
     siamese_model.fit_generator(data_gen, steps_per_epoch=10, epochs=200)
